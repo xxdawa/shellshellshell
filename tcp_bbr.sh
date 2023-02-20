6364cc4dd5d3fc31ad3f1dd9377f6557880b883d
xxdawa <105995579+xxdawa@users.noreply.github.com>
2023-02-21 03:18:07 +0800

    Update tcp_bbr.sh

--- tcp_bbr.sh
+++ tcp_bbr.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # 安装BBR加速
-# 需要root权限执行
+# 检查是否为Root
+[ $(id -u) != "0" ] && { colorEcho ${RED} "Error: You must be root to run this script"; exit 1; }
+# 下载并安装
 wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh"
 chmod +x tcp.sh
 ./tcp.sh
